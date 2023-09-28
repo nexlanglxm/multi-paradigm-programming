54d59f1a95f6632474a289883cdd85891c721cbc
Neil Anglim <G00425634@atu.ie>
2023-09-28 20:11:37 +0100

    1.3 now working properly

--- w1.3multi.py
+++ w1.3multi.py
@@ -1,16 +1,32 @@
 '''• Write a program that asks the user for a number n and gives them the possibility to choose between
 computing the sum and computing the product of 1,. . . ,n.'''
 # i will use functions to separate out the code and help it look more organized in case i need it again 
+def compute_sum(n):
+    return sum(range(1,n + 1))
+
+def compute_product(n):
+    result = 1
+    for i in range(1,n + 1):
+        result *= i
+    return result
+
 def main():
     try:
         n = int(input("Enter a number n: "))
         choice = input("Choose 'sum' or 'product':").lower()
 
-        result = 0
-
         if choice == 'sum':
+            result = compute_sum(n)
+            print(f"The sum of numbers from 1 to {n} is {result}")
 
-
+        elif choice == 'product':
+            result = compute_product(n)
+            print(f"The product of numbers from 1 to {n} is {result}")
+        else:
+            print("Invalid choice. Please choose 'sum' or 'product'.")
 
     except ValueError:
-        print("This input is invalid. Please enter a valid number")
+        print("Invalid input. Please enter a valid number.")
+    
+if __name__ == "__main__":
+    main()
