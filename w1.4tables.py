de0ce8f14afeced5fb0f6e2892ed5c1ef29e78e5
Neil Anglim <G00425634@atu.ie>
2023-09-28 20:23:52 +0100

    a nice tidy table :)

--- w1.4tables.py
+++ w1.4tables.py
@@ -1,1 +1,6 @@
 # Write a program that prints a multiplication table for numbers up to 12
+for i in range(1,13):
+    for j in range(1,13):
+        print(f"{i * j:4}", end="") 
+    print()
+    #the '4' specifies that each value should occupy a minimum of 4 characters, which works well for the first 999 numbers :)
