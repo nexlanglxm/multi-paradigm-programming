30515c227a9f24121c8a2344d18191c418a563cd
Neil Anglim <G00425634@atu.ie>
2023-09-28 20:34:57 +0100

    functioning sieve of eratosthenes

--- w1.5primes.py
+++ w1.5primes.py
@@ -1,1 +1,20 @@
 # Write a program that prints all prime numbers smaller than 100.
+# first, a function to define prime numbers
+def is_prime(n):
+    if n <= 1: 
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    
+    for i in range(3, int(n**0.5) + 1,2):
+        if n % i == 0:
+            return False
+        
+    return True
+
+print("all prime numbers smaller than 100:")
+for number in range(2,100):
+    if is_prime(number):
+        print(number,end=" ")
