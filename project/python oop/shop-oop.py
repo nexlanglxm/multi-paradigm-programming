7b7516e2e7b3c4fa1edbc5ebb794fc06b5dbbee6
Neil Anglim <neiljanglim@gmail.com>
2023-12-05 13:27:29 +0000

    updating the file, updating csv filepaths

--- project/python oop/shop-oop.py
+++ project/python oop/shop-oop.py
@@ -1,70 +1,102 @@
 import os
 import csv
 # change the working directory
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+# os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+class Product:
+# initialize the product with a name and a price
+    def __init__(self, name, price=0):
+        self.name = name
+        self.price = price
+# return the name & price of the product
+    def __repr__(self):
+        return f'NAME: {self.name} PRICE: {self.price}'
 
-
-class Product: 
-    def __init__(self, name, price, quantity): 
-        self.name = name 
-        self.price = price 
+class ProductStock:
+    
+    def __init__(self, product, quantity):
+        self.product = product
         self.quantity = quantity
-
-    def __str__(self): 
-       return f'NAME: {self.name}, PRICE: {self.price}, QUANTITY: {self.quantity}'
-    
-class Shop: 
-    def __init__(self, cash, products): 
-        self.cash = cash 
-        self.products = products
+# return the name of the product
+    def name(self):
+        return self.product.name;
+# return the unit price of the product
+    def unit_price(self):
+        return self.product.price;
+# calculate the cost of the product       
+    def cost(self):
+        return self.unit_price() * self.quantity
         
-    def add_product(self, product): 
-        self.products.append(product)
+    def __repr__(self):
+        return f"{self.product} QUANTITY: {self.quantity}"
+# initialize the customer with a shopping list
+class Customer:
+
+    def __init__(self, path):
+        self.shopping_list = []
+        with open(path) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            first_row = next(csv_reader)
+            self.name = first_row[0]
+            self.budget = float(first_row[1])
+            for row in csv_reader:
+                name = row[0]
+                quantity = float(row[1])
+                p = Product(name)
+                ps = ProductStock(p, quantity)
+                self.shopping_list.append(ps) 
+                
+    def calculate_costs(self, price_list):
+        for shop_item in price_list:
+            for list_item in self.shopping_list:
+                if (list_item.name() == shop_item.name()):
+                    list_item.product.price = shop_item.unit_price()
+    
+    def order_cost(self):
+        cost = 0
         
-    def __str__(self): 
-        result = f'INITIAL CASH: {self.cash}\n' 
-        for product in self.products: 
-            result += str(product) + '\n' 
-        return result
+        for list_item in self.shopping_list:
+            cost += list_item.cost()
+        
+        return cost
     
-class Customer: 
-    def __init__(self, name, cash, products): 
-        self.name = name 
-        self.cash = cash 
-        self.products = products
-
-    def add_product(self, product): 
-        self.products.append(product)
-
-    def __str__(self): 
-        result = f'NAME: {self.name}, CASH: {self.cash}\n' 
-        for product in self.products: 
-            result += f'NAME: {product.name}, QUANTITY: {product.quantity}\n' 
-        return result
+    def __repr__(self):
+        
+        str = f"{self.name} wants to buy"
+        for item in self.shopping_list:
+            cost = item.cost()
+            str += f"\n{item}"
+            if (cost == 0):
+                str += f" {self.name} doesn't know how much that costs :("
+            else:
+                str += f" COST: {cost}"
+                
+        str += f"\nThe cost would be: {self.order_cost()}, he would have {self.budget - self.order_cost()} left"
+        return str 
+        
+class Shop:
     
-def create_shop_and_stock(): 
-    products = [] 
-    with open("stock.csv") as csv_file: 
-        csv_reader = csv.reader(csv_file, delimiter=',') 
-        cash = float(next(csv_reader)[0]) 
-        for row in csv_reader: 
-            product = Product(row[0], float(row[1]), int(row[2])) 
-            products.append(product) 
-        return Shop(cash, products)
+    def __init__(self, path):
+        self.stock = []
+        with open(path) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            first_row = next(csv_reader)
+            self.cash = float(first_row[0])
+            for row in csv_reader:
+                p = Product(row[0], float(row[1]))
+                ps = ProductStock(p, float(row[2]))
+                self.stock.append(ps)
+    
+    def __repr__(self):
+        str = ""
+        str += f'Shop has {self.cash} in cash\n'
+        for item in self.stock:
+            str += f"{item}\n"
+        return str
 
-def read_customer(file_path): 
-    products = [] 
-    with open(file_path) as csv_file: 
-        csv_reader = csv.reader(csv_file, delimiter=',') 
-        name, cash = next(csv_reader) 
-        for row in csv_reader: 
-            product = Product(row[0], float(row[1]), 0) # Initialize quantity to 0 for customer 
-            products.append(product) 
-        return Customer(name, float(cash), products)
-# Create the shop and customer
-shop = create_shop_and_stock()
-customer = read_customer("customer.csv")
-# Print the shop and customer details
-print(shop)
-print(customer)
+s = Shop("../project/stock.csv")
+print(s)
+
+c = Customer("../project/customer.csv")
+c.calculate_costs(s.stock)
+print(c)
