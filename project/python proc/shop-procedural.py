bee312dbc4f103b955d00966cfc2cac1fdde6d29
Neil Anglim <neiljanglim@gmail.com>
2023-12-12 16:34:38 +0000

    update shop based on orders, fixing issues

--- project/python proc/shop-procedural.py
+++ project/python proc/shop-procedural.py
@@ -13,9 +13,9 @@ def create_and_stock_shop():
             shop["cash"] = float(first_row[0])
             shop["products"] = []
             for row in csv_reader:
-                if len(row) >= 3: #check if the row has at least 3 columns
+                if len(row) >= 3:  # check if the row has at least 3 columns
                     product = {}
-                    product["name"] = row[0]
+                    product["name"] = row[0].strip()  # Remove whitespace
                     product["price"] = float(row[1])
                     product["quantity"] = int(row[2])
                     shop["products"].append(product)
@@ -30,6 +30,7 @@ def create_and_stock_shop():
     return shop
 
 def process_customer_orders(customer, shop):
+    total_cost = 0
     for product in customer["products"]:
         product_name = product["name"]
         product_quantity = float(product["quantity"])  # Change int() to float()
@@ -43,37 +44,50 @@ def process_customer_orders(customer, shop):
                     break
                 else:
                     shop_product["quantity"] = str(float(shop_product["quantity"]) - product_quantity)  # Update to string
+                    total_cost += product_quantity * shop_product["price"]  # Add the cost of each product to the total cost
                     print(f"Order placed for {product_quantity} units of {product_name}.")
                     break
         
         if not found:
-            print(f"Error: Product {product_name} not found in the shop's inventory.")
+            print(f"Error: Product '{product_name}' not found in the shop's inventory.")
 
+    shop["cash"] += total_cost  # Increase the shop's cash by the total cost from the purchase
+    customer["cash"] -= total_cost  # Deduct the total cost from the customer's cash
 
+    print(f"Customer's total cost: {total_cost}")
+    print(f"Updated Shop's cash: {shop['cash']}")
+    print(f"Updated Customer's cash: {customer['cash']}")
+    print("Updated shop inventory: ")
+    for product in shop["products"]:
+        print(f'NAME: {product["name"]}, PRICE: {product["price"]}, QUANTITY: {product["quantity"]}')
+        
 def read_customer():
-    customer = {}
+    customers = []
     try:
         with open("./project/customer.csv") as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            first_row = next(csv_reader)
-            customer["name"] = first_row[0]
-            customer["cash"] = float(first_row[1])
-            customer["products"] = []
+            csv_reader = csv.reader(csv_file)
             for row in csv_reader:
-                if len(row) >= 2:
+                customer = {}
+                customer["name"] = row[0]
+                customer["cash"] = float(row[1])
+                customer["products"] = []
+                
+                # Process products and quantities for each customer
+                for i in range(2, len(row), 2):
                     product = {}
-                    product["name"] = row[0]
-                    product["quantity"] = row[1].strip()  # Remove leading/trailing whitespace
+                    product["name"] = row[i].strip() # Remove whitespace
+                    product["quantity"] = int(row[i + 1])
                     customer["products"].append(product)
-                else:
-                    print(f"Issue with row: {row}.")
+
+                customers.append(customer)  # Add the processed customer to the list
+
     except FileNotFoundError:
         print("File 'customer.csv' not found. Please check the file path.")
     except ValueError as ve:
         print(f"Error: Invalid data within cutomer.csv - {ve}")
     except Exception as e:
         print(f"An error occurred: {e}")
-    return customer
+    return customers # Return the list of customers
 
 
 def print_product(product):
@@ -86,13 +100,13 @@ def print_customer(customer):
 
 def print_shop(shop):
     print(f'INITIAL CASH: {shop["cash"]}')
+    print("Updated shop inventory: ")
     for product in shop["products"]:
-        print_product(product)
+        print(f'NAME: {product["name"]}, PRICE: {product["price"]}, QUANTITY: {product["quantity"]}')
 
 shop = create_and_stock_shop()
-print_shop(shop)
+print(shop)
 
-customer = read_customer()
-print_customer(customer)
-
-process_customer_orders(customer, shop)
+customers = read_customer()
+for customer in customers:
+    process_customer_orders(customer, shop)
